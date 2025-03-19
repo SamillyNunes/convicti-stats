054867acce20652fc174ed164dfac172a26c9994
Samilly <samillynunes19@gmail.com>
2025-03-19 20:47:56 -0300

    feat: handle logout and ConfirmDialog component created

---
+++ src/components/ConfirmDialog.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="fixed inset-0 bg-black/50 flex justify-center items-center z-50">
+    <div class="bg-white p-5 rounded-lg shadow-lg w-96">
+      <h2 class="font-bold text-lg">Confirmação</h2>
+
+      <p class="pb-10">Tem certeza que deseja sair?</p>
+
+      <div class="flex justify-end gap-2">
+        <button
+          @click="onCancel"
+          class="px-4 py-2 bg-main-purple text-white rounded hover:bg-main-purple/70 duration-200"
+        >
+          Cancelar
+        </button>
+        <button
+          @click="onConfirm"
+          class="px-4 py-2 bg-gray-400 rounded hover:bg-gray-400/70 duration-200"
+        >
+          Sair
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  onCancel: () => void
+  onConfirm: () => void
+}>()
+</script>
--- src/components/SideMenu.vue
+++ src/components/SideMenu.vue
@@ -7,9 +7,21 @@
     </div>
     <div class="flex flex-col items-center">
       <MenuButton title="Configurações" :icon-url="settingsIcon" :is-active="false" />
-      <MenuButton title="Sair" :icon-url="exitIcon" :is-active="false" />
+      <MenuButton
+        @click="openDialog"
+        class="hover:bg-error/40"
+        title="Sair"
+        :icon-url="exitIcon"
+        :is-active="false"
+      />
     </div>
   </div>
+
+  <ConfirmDialog
+    v-if="isDialogOpen"
+    :onCancel="() => (isDialogOpen = false)"
+    :onConfirm="handleLogout"
+  />
 </template>
 
 <script setup lang="ts">
@@ -17,4 +29,22 @@ import MenuButton from './MenuButton.vue'
 import dashboardIcon from '@/assets/icons/dashboard.png'
 import settingsIcon from '@/assets/icons/settings.png'
 import exitIcon from '@/assets/icons/leave.png'
+import { useAuthStore } from '@/stores/auth'
+import { useRouter } from 'vue-router'
+import ConfirmDialog from './ConfirmDialog.vue'
+import { ref } from 'vue'
+
+const router = useRouter()
+
+const isDialogOpen = ref(false)
+
+const openDialog = () => {
+  isDialogOpen.value = true
+}
+
+const handleLogout = () => {
+  const authStore = useAuthStore()
+  authStore.logoutUser()
+  router.push('/login')
+}
 </script>
