67196ad4cc95b8dfa3a8f74d51909f2509d1b687
Samilly <samillynunes19@gmail.com>
2025-03-21 11:56:14 -0300

    style: adjusted Card and Layout components when in mobile

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-white w-full !py-5 !px-10 !mt-5 rounded-lg overflow-auto">
+  <div class="bg-white w-full py-5 px-5 md:px-10 mt-5 rounded-lg overflow-auto">
     <slot></slot>
   </div>
 </template>
--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -2,7 +2,7 @@
   <div class="w-screen h-screen">
     <div class="flex md:w-full md:h-full">
       <SideMenu />
-      <div class="flex flex-col h-full w-full bg-gray-100 !py-5 !px-10">
+      <div class="flex flex-col h-full w-full bg-gray-100 py-5 px-7 md:px-10">
         <slot></slot>
       </div>
     </div>
